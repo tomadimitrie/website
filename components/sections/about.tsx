2ad23ff82b73535f65c7994be47ab7f5be32e124
Dimitrie-Toma Furdui <sign@tomadimitrie.dev>
2026-07-15 16:13:51 +0300

    added cv compilation

--- components/sections/about.tsx
+++ components/sections/about.tsx
@@ -4,6 +4,9 @@ import { CONFIG } from "@/lib/config";
 import { ShellText } from "../shell-text";
 
 export function AboutSection() {
+  const buttonStyle =
+    "text-primary-foreground text-2xl font-mono font-bold border-primary-foreground border-2 w-min px-10 py-1 rounded-md hover:underline hover:bg-primary-foreground/30 ml-auto";
+
   return (
     <SectionWrapper>
       <div className="min-h-[calc(100vh-var(--spacing)*30)] flex flex-col justify-center gap-6">
@@ -18,21 +21,29 @@ export function AboutSection() {
           <div className="text-muted-foreground text-md md:text-lg lg:text-xl">
             {CONFIG.about}
           </div>
-          <div className="flex items-center gap-3">
-            {CONFIG.socials.map(({ url, Icon }) => (
-              <Link key={url} target="_blank" href={url}>
-                <Icon
-                  className="text-muted-foreground hover:text-primary-foreground"
-                  size={30}
-                />
+          <div className="flex flex-col md:flex-row items-center md:justify-between gap-3">
+            <div className="flex items-center gap-5">
+              {Object.values(CONFIG.socials)
+                .filter((social) =>
+                  "hidden" in social ? !social.hidden : true,
+                )
+                .map(({ url, Icon }) => (
+                  <Link key={url} target="_blank" href={url}>
+                    <Icon
+                      className="text-muted-foreground hover:text-primary-foreground"
+                      size={30}
+                    />
+                  </Link>
+                ))}
+            </div>
+            <div className="flex items-center gap-5">
+              <Link className={buttonStyle} href="/blog">
+                Blog
               </Link>
-            ))}
-            <Link
-              className="text-primary-foreground text-2xl font-mono font-bold border-primary-foreground border-2 w-min px-10 py-1 rounded-md hover:underline hover:bg-primary-foreground/30 ml-auto"
-              href="/blog"
-            >
-              Blog
-            </Link>
+              <Link className={buttonStyle} href="/cv" target="_blank">
+                CV
+              </Link>
+            </div>
           </div>
         </div>
       </div>
